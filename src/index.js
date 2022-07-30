3ec293ad4c01d49206bd2267bcb7c4a9ead7f75b
Andres Gordon <gordon_andres@yahoo.es>
2022-07-29 19:25:35 -0500

    Added main module file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import navBar from './templates/navBar';
+import searchContainer from './templates/searchContainer';
+import flagsContainer from './templates/cardsContainer';
+
+(async () => {
+  const App = document.getElementById('App');
+  App.innerHTML += navBar();
+  App.innerHTML += searchContainer();
+  App.innerHTML += await flagsContainer();
+})()
