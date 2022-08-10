9ba2b2b21449210eed39b33846bf85be5414014c
Andres Gordon <gordon_andres@yahoo.es>
2022-08-09 21:35:07 -0500

    Removed header for adding to template html file
    Header component removed from the file for implementing into the
    template html file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import navBar from './templates/navBar';
 import searchContainer from './templates/searchContainer';
 import flagsContainer from './templates/cardsContainer';
 
 (async () => {
   const App = document.getElementById('App');
-  App.innerHTML += navBar();
   App.innerHTML += searchContainer();
   App.innerHTML += await flagsContainer();
 })()
